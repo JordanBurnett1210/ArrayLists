b60541528b68a268bd53774896332a8fb7c58fc6
JordanBurnett1210 <jabur1210@gmail.com>
2015-10-07 09:56:15 -0600

    added lists.

--- src/lists/controller/ArrayListsController.java
+++ src/lists/controller/ArrayListsController.java
@@ -1,10 +1,31 @@
 package lists.controller;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+import javax.swing.JOptionPane;
+
 public class ArrayListsController
 {
+	private ArrayList<String> firstWords;
+	
+	public ArrayListsController()
+	{
+		firstWords = new ArrayList<String>();
+	}
 	
 	public void start()
 	{
-		
+		firstWords.add("jedininjasamuraigobblygukegrandpuba!");
+		firstWords.add("babababababababababababababababababababababababababababababababababababa");
+		showList();
+	}
+	
+	private void showList()
+	{
+		for(int spot =0; spot < firstWords.size(); spot++)
+		{
+			JOptionPane.showMessageDialog(null,  firstWords.get(spot) + " is at spot# " + spot);;
+		}
 	}
 }
